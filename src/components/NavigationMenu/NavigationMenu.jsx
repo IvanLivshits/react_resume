6d76a179903b5d696af94826de6dbec173508c2f
Ivan Livshits <ivanlivshitc@gmail.com>
2022-09-20 16:40:18 +0300

    fix: styles for navigation menu feat: add styles in the component delete: module.css for navigation menu

--- src/components/NavigationMenu/NavigationMenu.jsx
+++ src/components/NavigationMenu/NavigationMenu.jsx
@@ -1,39 +1,165 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
-import { MdMenu, MdClose } from 'react-icons/md';
-import classes from './NavigationMenu.module.css';
+import styled from 'styled-components';
+import { MdClose, MdMenu } from 'react-icons/md';
 
-const NavigationMenu = () => (
-  <div className={classes.NavMenu}>
-    <div className={classes.mobileMenuIcon}>
-      <MdMenu />
-    </div>
-    <ul className={classes.ul}>
-      <div className={classes.closeNavIcon}>
-        <MdClose />
+const NavigationStyles = styled.nav`
+  position: fixed;
+  z-index: 100;
+  top: 0;
+  left: 0;
+  width: 100%;
+  padding: 1rem 0;
+  background: var(--dark-bg);
+  ul {
+    max-width: 1200px;
+    margin: 0 auto;
+    width: 90%;
+    text-align: center;
+    li {
+      display: inline-block;
+      border-radius: 8px;
+      transition: 0.3s ease background-color;
+      &:hover {
+        background-color: var(--deep-dark);
+      }
+    }
+    a {
+      display: inline-block;
+      font-family: 'RobotoMono Regular';
+      padding: 1rem 2rem;
+      font-size: 2rem;
+      color: var(--gray-1);
+      outline: none;
+    }
+    .active {
+      color: var(--white);
+    }
+  }
+  .mobile-menu-icon {
+    position: absolute;
+    right: 1rem;
+    top: 1rem;
+    width: 4rem;
+    cursor: pointer;
+    display: none;
+    outline: none;
+    * {
+      pointer-events: none;
+    }
+  }
+  .navItems .closeNavIcon {
+    display: none;
+  }
+  @media only screen and (max-width: 768px) {
+    padding: 0;
+    .hide-item {
+      transform: translateY(calc(-100% - var(--top)));
+    }
+    .mobile-menu-icon {
+      display: block;
+    }
+    .navItems {
+      --top: 1rem;
+      transition: 0.3s ease transform;
+      background-color: var(--deep-dark);
+      padding: 2rem;
+      width: 90%;
+      max-width: 300px;
+      border-radius: 12px;
+      position: absolute;
+      right: 1rem;
+      top: var(--top);
+      .closeNavIcon {
+        display: block;
+        width: 3rem;
+        margin: 0 0 0 auto;
+        cursor: pointer;
+        * {
+          pointer-events: none;
+        }
+      }
+      li {
+        display: block;
+        margin-bottom: 1rem;
+      }
+    }
+  }
+`;
+
+const NavigationMenu = () => {
+  const [showNavigation, setShowNavigation] = useState(false);
+
+  return (
+    <NavigationStyles>
+      <div
+        className="mobile-menu-icon"
+        onClick={() => setShowNavigation(!showNavigation)}
+        role="button"
+        onKeyDown={() => setShowNavigation(!showNavigation)}
+        tabIndex={0}
+      >
+        <MdMenu />
       </div>
-      <li className={classes.li}>
-        <NavLink className={classes.a} to="/" exact>
-          Home
-        </NavLink>
-      </li>
-      <li className={classes.li}>
-        <NavLink className={classes.a} to="/about">
-          About
-        </NavLink>
-      </li>
-      <li className={classes.li}>
-        <NavLink className={classes.a} to="/projects">
-          Projects
-        </NavLink>
-      </li>
-      <li className={classes.li}>
-        <NavLink className={classes.a} to="/contacts">
-          Contacts
-        </NavLink>
-      </li>
-    </ul>
-  </div>
-);
+
+      <ul className={!showNavigation ? 'navItems hide-item' : 'navItems'}>
+        <div
+          className="closeNavIcon"
+          onClick={() => setShowNavigation(!showNavigation)}
+          role="button"
+          onKeyDown={() => setShowNavigation(!showNavigation)}
+          tabIndex={0}
+        >
+          <MdClose />
+        </div>
+        <li>
+          <NavLink
+            to="/"
+            exact
+            onClick={() => setShowNavigation(!showNavigation)}
+            role="button"
+            onKeyDown={() => setShowNavigation(!showNavigation)}
+            tabIndex={0}
+          >
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/about"
+            onClick={() => setShowNavigation(!showNavigation)}
+            role="button"
+            onKeyDown={() => setShowNavigation(!showNavigation)}
+            tabIndex={0}
+          >
+            About
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/projects"
+            onClick={() => setShowNavigation(!showNavigation)}
+            role="button"
+            onKeyDown={() => setShowNavigation(!showNavigation)}
+            tabIndex={0}
+          >
+            Projects
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/contact"
+            onClick={() => setShowNavigation(!showNavigation)}
+            role="button"
+            onKeyDown={() => setShowNavigation(!showNavigation)}
+            tabIndex={0}
+          >
+            Contact
+          </NavLink>
+        </li>
+      </ul>
+    </NavigationStyles>
+  );
+};
 
 export default NavigationMenu;
--- src/index.js
+++ src/index.js
@@ -5,10 +5,10 @@ import GlobalStyles from './styles/GlobalStyle';
 import Typography from './styles/Typography';
 
 ReactDOM.render(
-  <>
+  <div>
     <GlobalStyles />
     <Typography />
     <App />
-  </>,
+  </div>,
   document.getElementById('root')
 );
